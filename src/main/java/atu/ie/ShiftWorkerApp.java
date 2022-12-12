133caea82616349fa59ecfcf9df8ae43e3ec1bf2
Parthibg00410769 <g00410769@atu.ie>
2022-12-12 10:42:13 +0000

    first one again again

---
+++ src/main/java/atu/ie/ShiftWorkerApp.java
@@ -0,0 +1,14 @@
+package atu.ie;
+
+
+
+public class ShiftWorkerApp {
+    public static void main(String[] args) {
+
+        ShiftWorker Parthib = new ShiftWorker("ghgfsdg", "fdgdf", 1,19 );
+        System.out.println(Parthib.toString());
+
+
+
+    }
+}
